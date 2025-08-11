579e98cb00d4ee52e36dae2f5b6a56bae00d3813
Shipra <138140065+Shi-pra-19@users.noreply.github.com>
2025-08-11 05:46:26 +0000

    requirements

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = '23f3001910'
+    SECRET_KEY = 'secret-key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
